432e5c1e5294e9edc6f0afa37d16b13867c60520
qianc <qianc@zbu.com>
2023-01-02 19:37:46 +0800

    测试GitHub提交

---
+++ common/common-util/src/main/java/com/zbu/Test.java
@@ -0,0 +1,16 @@
+package com.zbu;
+
+/**
+ * ClassName: Test
+ * Package: com.zbu
+ * Description:
+ *
+ * @Author: 冯振卓
+ * @Create: 2023/1/2 - 19:34
+ * @Version: v1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("11111111");
+    }
+}
---
+++ service-system/src/main/java/com/zbu/Main.java
@@ -0,0 +1,16 @@
+package com.zbu;
+
+/**
+ * ClassName: ${NAME}
+ * Package: com.zbu
+ * Description:
+ *
+ * @Author: 冯振卓
+ * @Create: ${DATE} - ${TIME}
+ * @Version: v1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
